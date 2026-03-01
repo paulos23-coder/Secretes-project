afbb1816189f4fc335e9017c7900516151382a94
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-03-01 10:45:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+import bodyParser from "body-parser";
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}.`);
+});
