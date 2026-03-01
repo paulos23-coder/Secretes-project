189b54e7cf7b2cb8910dd4f28d95d97b9d41e392
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-03-01 21:06:52 +0300

    Connected to mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import ejs from "ejs";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
@@ -13,12 +14,36 @@ app.use(
     extended: true,
   }),
 );
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+  username: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const newUser = User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  try {
+    await newUser.save();
+    res.render("secrets.ejs");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get("/login", (req, res) => {
